ee2ff692e2ee9bd5e4485251d67e8f903db80ca7
Amita1305 <ami13may@gmail.com>
2023-01-22 08:24:16 +0530

    Amita:AddingCucumberProject

---
+++ src/test/java/Com/itview/testcases/selenium_cucumber_test/AppTest.java
@@ -0,0 +1,20 @@
+package Com.itview.testcases.selenium_cucumber_test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
